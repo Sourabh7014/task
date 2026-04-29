380823de52f80ccd7815a072b6c04ea78447bbac
Sourabh Jangir <sourabh786784@gmail.com>
2026-04-29 13:20:23 +0530

    update some point

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class SuperAdminSeeder extends Seeder
@@ -15,14 +14,13 @@ class SuperAdminSeeder extends Seeder
     {
         $password = Hash::make('password');
         $now = now();
-
-        DB::statement("INSERT INTO users (name, email, password, role, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)", [
-            'Super Admin',
-            'superadmin@example.com',
-            $password,
-            'SuperAdmin',
-            $now,
-            $now
+        $admin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@example.com',
+            'password' => $password,
+            'role' => 'SuperAdmin',
+            'created_at' => $now,
+            'updated_at' => $now,
         ]);
     }
 }
